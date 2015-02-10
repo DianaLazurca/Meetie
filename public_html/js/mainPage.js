dede55a36e24f48b18706931efd496452e97de19
DianaLazurca <dianna.lazurca@gmail.com>
2015-02-10 19:35:10 +0200

    twitter notifications

--- public_html/js/mainPage.js
+++ public_html/js/mainPage.js
@@ -3,9 +3,11 @@ function notifyDirectMessages(loggedUser) {
     localStorage.putObjectInLocalStorage('lastMessageSeenOnApp', directMessages[0]["id"]);
     var lastLogout = localStorage.getObjectGromLocalStorage('lastLogout');
     var lastUnloadPage = localStorage.getObjectGromLocalStorage('closeTab');
-    logoutDate = new Date(lastLogout);
-    closeTabDate = new Date(lastUnloadPage);
+    var logoutDate = new Date(lastLogout);
+    var closeTabDate = new Date(lastUnloadPage);
     var lastOut = logoutDate.getTime() < closeTabDate.getTime() ? closeTabDate : logoutDate;
+
+
     for (var i = 0; i < directMessages.length; i++) {
         if (directMessages[i]["sender_screen_name"] !== loggedUser["alias"]) {
             if (lastOut.getTime() < new Date(directMessages[i]["created_at"]).getTime()) {
@@ -13,24 +15,71 @@ function notifyDirectMessages(loggedUser) {
                         directMessages[i]["sender"]["name"] +
                         "[@" + directMessages[i]["sender_screen_name"] + "] said " + directMessages[i]["text"] + " " + '<br/>';
                 $("#directMessages").append(txt);
-                    
+
             }
         }
     }
 }
+
+function notifyDirectMessagesOnline(loggedUser) {
+    var lastMessageID = localStorage.getObjectGromLocalStorage('lastMessageSeenOnApp');
+
+    var twitterService = authorizeAndInitializeService().twitterService;
+    twitterService.getUserDirectMessagesGreaterThanId(lastMessageID);
+
+    window.setTimeout(function () {
+        var newMessages = localStorage.getObjectGromLocalStorage('directMessagesListGreater');
+
+        if (newMessages !== null && newMessages.length !== 0) {
+            localStorage.putObjectInLocalStorage('lastMessageSeenOnApp', newMessages[0]["id"]);
+            for (var i = 0; i < newMessages.length; i++) {
+                if (newMessages[i]["sender_screen_name"] !== loggedUser["alias"]) {
+                    var txt = "ID " + newMessages[i]["id"] + " -- DATE " + newMessages[i]["created_at"] + " " +
+                            newMessages[i]["sender"]["name"] +
+                            "[@" + newMessages[i]["sender_screen_name"] + "] said " + newMessages[i]["text"] + " " + '<br/>';
+                    $("#directMessages").append(txt);
+                }
+            }
+        }
+
+
+    }, 2000);
+
+}
 function prepareTweets(twitterService) {
     var mentions = localStorage.getObjectGromLocalStorage('mentionsList');
-    localStorage.putObjectInLocalStorage('lastMention', mentions[0]["id_str"]);
-    for (var i = 0; i < mentions.length; i++) {
-        twitterService.displayTwitterFormat(mentions[i]["id_str"]);
+
+    if (mentions !== null && mentions.length !== 0) {
+        localStorage.putObjectInLocalStorage('lastMention', mentions[0]["id_str"]);
+        for (var i = 0; i < mentions.length; i++) {
+            twitterService.displayTwitterFormat(mentions[i]["id_str"]);
+        }
     }
+
+}
+
+function prepareTweetsOnline(twitterService) {
+    var lastMentionFromUser = localStorage.getObjectGromLocalStorage('lastMention');
+
+    twitterService.getUserMentionsGreaterThanId(lastMentionFromUser);
+
+    window.setTimeout(function () {
+        var newMentions = localStorage.getObjectGromLocalStorage('mentionsListGreater');
+        if (newMentions !== null && newMentions.length !== 0) {
+            localStorage.putObjectInLocalStorage('lastMention', newMentions[0]["id_str"]);
+            for (var i = 0; i < newMentions.length; i++) {
+                twitterService.displayTwitterFormat(newMentions[i]["id_str"]);
+            }
+        }
+    }, 2000);
+
 }
 function notifyMentions() {
     var mentions = localStorage.getObjectGromLocalStorage('mentionsList');
     var lastLogout = localStorage.getObjectGromLocalStorage('lastLogout');
     var lastUnloadPage = localStorage.getObjectGromLocalStorage('closeTab');
-    logoutDate = new Date(lastLogout);
-    closeTabDate = new Date(lastUnloadPage);
+    var logoutDate = new Date(lastLogout);
+    var closeTabDate = new Date(lastUnloadPage);
     var lastOut = logoutDate.getTime() < closeTabDate.getTime() ? closeTabDate : logoutDate;
 
     for (var i = 0; i < mentions.length; i++) {
@@ -42,4 +91,34 @@ function notifyMentions() {
 
     }
 
+}
+
+function notifyMentionsOnline() {
+
+    window.setTimeout(function () {
+        var newMentions = localStorage.getObjectGromLocalStorage('mentionsListGreater');
+        if (newMentions !== null && newMentions.length !== 0) {
+            for (var i = 0; i < newMentions.length; i++) {
+                var format = localStorage.getObjectGromLocalStorage('format'+newMentions[i]["id_str"]);
+                localStorage.removeItem('format' + newMentions[i]["id_str"]);
+                $("#mentions").append(format["html"]);
+            }
+        }
+
+    }, 2000);
+
+
+
+
+}
+
+function setUpdateInterval(notifyDirectMessagesOnline, loggedUser, prepareTweetsOnline, twitterService, notifyMentionsOnline) {
+    setInterval(function () {
+        notifyDirectMessagesOnline(loggedUser);
+        prepareTweetsOnline(twitterService);
+        window.setTimeout(function () {
+             notifyMentionsOnline();
+        }, 2000);
+       
+    }, 60000);
 }
--- public_html/js/meetie.js
+++ public_html/js/meetie.js
@@ -1,211 +1,215 @@
-var meetie = {  
-   
-  UserModel :{ 
-    User : function (id, screen_name, name, alias, picture){ 
-    
-    this.id = id;
-    this.screen_name = screen_name;
-    this.name = name;
-    this.alias = alias;
-    this.picture = picture;
-    this.friends = new Array(0);
-    this.followers = new Array(0);
-	
+var meetie = {
+    UserModel: {
+        User: function (id, screen_name, name, alias, picture) {
+
+            this.id = id;
+            this.screen_name = screen_name;
+            this.name = name;
+            this.alias = alias;
+            this.picture = picture;
+            this.friends = new Array(0);
+            this.followers = new Array(0);
+
+        }
+    },
+    TwitterServiceModel: {
+        TwitterService: function (oauthResult) {
+            this.oauthResult = oauthResult;
+
+        }
     }
-},
-  TwitterServiceModel : {
-    TwitterService: function(oauthResult ) {
-      this.oauthResult = oauthResult;
-           
-    }
-  }  
-    
+
 };
 
 meetie.UserModel.User.prototype = {
-  getId: function(){
-    return this.id;
-  },
-  setFriends: function(friendsList){
-      this.friends = friendsList;
-  },
-  setFollowers: function(followersList){
-      this.followers = followersList;
-  },
-  getScreenName: function(){
-    return this.screen_name;
-  },
-  getName: function(){
-    return this.name;
-  },
-  getAlias: function(){
-    return this.alias;
-  },
-  getPicture: function(){
-    return this.picture;
-  },
-  getFriends: function(){
-    return this.friends;
-  },
-  getFollowers: function(){
-    return this.followers;
-  }
-  
-  
+    getId: function () {
+        return this.id;
+    },
+    setFriends: function (friendsList) {
+        this.friends = friendsList;
+    },
+    setFollowers: function (followersList) {
+        this.followers = followersList;
+    },
+    getScreenName: function () {
+        return this.screen_name;
+    },
+    getName: function () {
+        return this.name;
+    },
+    getAlias: function () {
+        return this.alias;
+    },
+    getPicture: function () {
+        return this.picture;
+    },
+    getFriends: function () {
+        return this.friends;
+    },
+    getFollowers: function () {
+        return this.followers;
+    }
+
+
 };
 
 meetie.TwitterServiceModel.TwitterService.prototype = {
-  getOauthResult: function(){
-    return this.oauthResult;
-  },
-  friendsList: function(id){
-    var url = "https://api.twitter.com/1.1/friends/list.json?user_id="+id;
-    this.getOauthResult().get(url).done(function(data) {
-            localStorage.putObjectInLocalStorage('friendsList', data); 
-          
+    getOauthResult: function () {
+        return this.oauthResult;
+    },
+    friendsList: function (id) {
+        var url = "/1.1/friends/list.json?user_id=" + id;
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('friendsList', data);
+            console.log(data);
+
         }).fail(function (err) {
             console.log("Failed to get all friends for " + id);
         });
-        
+
     },
-   followersList: function(id){
-        var url = "https://api.twitter.com/1.1/followers/list.json?user_id="+id;
-        this.getOauthResult().get(url).done(function(data) {
-                localStorage.putObjectInLocalStorage('followersList', data); 
-            }).fail(function (err) {
-                console.log("Failed to get all followers for " + id);
+    followersList: function (id) {
+        var url = "/1.1/followers/list.json?user_id=" + id;
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('followersList', data);
+        }).fail(function (err) {
+            console.log("Failed to get all followers for " + id);
         });
-        
+
     },
-    postMessage:function(status){
+    postMessage: function (status) {
         result.post('/1.1/statuses/update.json', {
-                         data: {
-                                status: status
-                            }
-                }).done(function(data) {
-                     // what to do with all I receive  
-                }).fail(function(err) {
-                        console.log("Failed to tweet");
-                });
-    },
-    getUserDirectMessages:function(id){
-        var url = "https://api.twitter.com/1.1/direct_messages.json";
-        this.getOauthResult().get(url).done(function(data) {               
-                localStorage.putObjectInLocalStorage('directMessagesList', data); 
-            }).fail(function (err) {
-                console.log("Failed to get all messages for " + id);
+            data: {
+                status: status
+            }
+        }).done(function (data) {
+
+        }).fail(function (err) {
+            console.log("Failed to tweet");
         });
     },
-    getUserMentions:function(id){
-        var url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
-        this.getOauthResult().get(url).done(function(data) {
-                localStorage.putObjectInLocalStorage('mentionsList', data); 
-            }).fail(function (err) {
-                console.log("Failed to get all mentions for " + id);
+    getUserDirectMessages: function (id) {
+        var url = "/1.1/direct_messages.json";
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('directMessagesList', data);
+        }).fail(function (err) {
+            console.log("Failed to get all messages for " + id);
         });
     },
-    getUserTweets:function(id){
-        var url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-        this.getOauthResult().get(url).done(function(data) {
-                localStorage.putObjectInLocalStorage('userTweets', data); 
-            }).fail(function (err) {
-                console.log("Failed to get all mentions for " + id);
+    getUserDirectMessagesGreaterThanId: function (id) {
+        var url = "/1.1/direct_messages.json?since_id=" + id;
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('directMessagesListGreater', data);
+        }).fail(function (err) {
+            console.log("Failed to get all messages for " + id);
         });
     },
-    displayTwitterFormat:function(id){
-        var url = "https://api.twitter.com/1.1/statuses/oembed.json?id="+id;
-        this.getOauthResult().get(url).done(function(data) {
-                localStorage.putObjectInLocalStorage("format"+id,data);
-            }).fail(function (err) {
-                console.log("Failed to get the tweet with id :  " + id);
+    getUserMentions: function (id) {
+        var url = "/1.1/statuses/mentions_timeline.json";
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('mentionsList', data);
+        }).fail(function (err) {
+            console.log("Failed to get all mentions for " + id);
         });
     },
-    logoutTwitter: function(){
-                OAuth.clearCache('twitter');
-                localStorage.clear();
-                localStorage.putObjectInLocalStorage('lastLogout',getCurrentDateTime());
-                console.log(" logout " + getCurrentDateTime());
-                window.location.href = "index.html";
-                
+    getUserMentionsGreaterThanId: function (id) {
+        var url = "/1.1/statuses/mentions_timeline.json?since_id=" + id;
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('mentionsListGreater', data);
+        }).fail(function (err) {
+            console.log("Failed to get all mentions greaterThan " + id);
+        });
     },
-    twitterLogin: function(){
+    getUserTweets: function (id) {
+        var url = "/1.1/statuses/user_timeline.json";
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage('userTweets', data);
+        }).fail(function (err) {
+            console.log("Failed to get all mentions for " + id);
+        });
+    },
+    displayTwitterFormat: function (id) {
+        var url = "/1.1/statuses/oembed.json?id=" + id;
+        this.getOauthResult().get(url).done(function (data) {
+            localStorage.putObjectInLocalStorage("format" + id, data);
+        }).fail(function (err) {
+            console.log("Failed to get the tweet with id :  " + id);
+        });
+    },
+    logoutTwitter: function () {
+        OAuth.clearCache('twitter');
+        localStorage.clear();
+        localStorage.putObjectInLocalStorage('lastLogout', getCurrentDateTime());
+        console.log(" logout " + getCurrentDateTime());
+        window.location.href = "index.html";
+
+    },
+    twitterLogin: function () {
         OAuth.popup('twitter', {cache: true}).done(function (twitter) {
-        twitter.me().done(function(data) {
-                localStorage.putObjectInLocalStorage('loggedinUser',data);
+            twitter.me().done(function (data) {
+                localStorage.putObjectInLocalStorage('loggedinUser', data);
                 twitterService = authorizeAndInitializeService().twitterService;
                 twitterService.friendsList(data["id"]);
                 twitterService.followersList(data["id"]);
+
+                console.log(data);
                 twitterService.getUserMentions(data["id"]);
                 twitterService.getUserDirectMessages(data["id"]);
                 twitterService.getUserTweets(data["id"]);
-                
+
                 window.setTimeout(function () {
-                        window.location.href = "mainPage.html";
+                     window.location.href = "mainPage3.html";
                 }, 1000);
-            }).fail(function(err){
+            }).fail(function (err) {
                 console.log(err);
             });
 
         }).fail(function () {
-                alert("Twitter authentication failed. Cannot provide results from Twitter. Please try again.");
+            alert("Twitter authentication failed. Cannot provide results from Twitter. Please try again.");
         });
-        
+
     }
-  
+
 };
 
 
-Storage.prototype.putObjectInLocalStorage = function(key, value) {
+Storage.prototype.putObjectInLocalStorage = function (key, value) {
     this.setItem(key, JSON.stringify(value));
 };
 
-Storage.prototype.getObjectGromLocalStorage = function(key) {
+Storage.prototype.getObjectGromLocalStorage = function (key) {
     var value = this.getItem(key);
     return  JSON.parse(value);
 };
 
-function authorizeAndInitializeService(){
+function authorizeAndInitializeService() {
     OAuth.initialize('NxfQddkW2whsh0-6HcSso1UvVYw');
     var twitter = OAuth.create('twitter');
-    return { 'twitter' : twitter, 'twitterService': new meetie.TwitterServiceModel.TwitterService(twitter) };
+    return {'twitter': twitter, 'twitterService': new meetie.TwitterServiceModel.TwitterService(twitter)};
 }
 
-function parseList( list){
+function parseList(list) {
     var friendList = new Array(0);
-    for (var user in list["users"]){
-        //console.log(list["users"][user]);
-         var userFriend = initializeUser(list["users"][user]["id"],list["users"][user]["screen_name"],
-                                         list["users"][user]["name"],list["users"][user]["alias"], 
-                                         list["users"][user]["profile_image_url"]);
-         friendList.push(userFriend);
-        
+    for (var user in list["users"]) {
+        var userFriend = initializeUser(list["users"][user]["id"], list["users"][user]["screen_name"],
+                list["users"][user]["name"], list["users"][user]["alias"],
+                list["users"][user]["profile_image_url"]);
+        friendList.push(userFriend);
+
     }
     return friendList;
 }
 
-function initializeUser(id,name,screen_name,alias, picture, friendsList,followersList){
-     var userFriend = new meetie.UserModel.User(id,screen_name,name,alias,picture);
-     userFriend.setFriends(friendsList);
-     userFriend.setFollowers(followersList);
-     
-     return userFriend;
+function initializeUser(id, name, screen_name, alias, picture, friendsList, followersList) {
+    var userFriend = new meetie.UserModel.User(id, screen_name, name, alias, picture);
+    userFriend.setFriends(friendsList);
+    userFriend.setFollowers(followersList);
+
+    return userFriend;
 }
-function convertToDate( string){
-  return new Date(string).getTime();
+function convertToDate(string) {
+    return new Date(string).getTime();
 }
-function getCurrentDateTime(){
- 
-    /*var objToday = new Date(),
-        weekday = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'),
-        dayOfWeek = weekday[objToday.getDay()],       
-        dayOfMonth = today + (objToday.getDate() < 10) ? '0'  : objToday.getDate(),
-        months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
-        curMonth = months[objToday.getMonth()],
-        curYear = objToday.getFullYear(),
-        curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
-        curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
-        curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds()
-        var today = dayOfWeek + " " + curMonth + " " +  dayOfMonth +" " + curHour + ":" + curMinute + ":" + curSeconds + " " +curYear ;
-    return today; */
+function getCurrentDateTime() {
     return new Date().getTime();
 }
